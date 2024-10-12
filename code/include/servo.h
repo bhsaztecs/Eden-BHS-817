66014e70a968b4c15f5064dfb7f1ed399c4405d5
Samiyel Frazier <samiyelfrazier@gmail.com>
2024-10-11 19:29:09 -0700

    General Upkeep
    Moved some files around, changed the license to AGPL
    added a gitignore file to ignore compilation tests. renamed main.cpp to example.cpp, says deleted.
    dropped the deed cus it wasnt necessary under AGPL.

--- code/include/servo.h
+++ code/include/servo.h
@@ -1,14 +1,13 @@
 #pragma once
-#include "Basic.h"
+#include "basic.h"
 
-void servos::Set(float armAngle, float clawAngle = TTDC(get_servo_position(ClawServo))) {
-  DLOG
-    set_servo_position(ArmServo, DTTA(armAngle));
+void servos::Set(float armAngle,
+                 float clawAngle = TTDC(get_servo_position(ClawServo))) {
+  DLOG set_servo_position(ArmServo, DTTA(armAngle));
   set_servo_position(ClawServo, DTTC(clawAngle));
 }
 void servos::Change(float changeInArmAngle, float changeInClawAngle = 0) {
-  DLOG
-    int curA = TTDA(get_servo_position(ArmServo));
+  DLOG int curA = TTDA(get_servo_position(ArmServo));
   int curC = TTDC(get_servo_position(ClawServo));
   int posA = curA + changeInArmAngle;
   int posC = curC + changeInClawAngle;
@@ -16,18 +15,16 @@ void servos::Change(float changeInArmAngle, float changeInClawAngle = 0) {
   servos::Set(posA, posC);
 }
 void servos::Move(float armAngle, float timeInSeconds) {
-  DLOG
-    int curAng = get_servo_position(ArmServo);
+  DLOG int curAng = get_servo_position(ArmServo);
   armAngle = DTTA(armAngle);
   int it = armAngle - curAng;
-  if ( it > 0 ) {
-    for ( int a = curAng; a < armAngle; a += 10 ) {
+  if (it > 0) {
+    for (int a = curAng; a < armAngle; a += 10) {
       Set(TTDA(a));
       Wait(abs(timeInSeconds / it) * 10);
     }
-  }
-  else {
-    for ( int a = curAng; a > armAngle; a -= 10 ) {
+  } else {
+    for (int a = curAng; a > armAngle; a -= 10) {
       Set(TTDA(a));
       Wait(abs(timeInSeconds / it) * 10);
     }
