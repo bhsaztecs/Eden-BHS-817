0bf37093f88c2ebca6015b845e268f51422780c2
Samiyel D Frazier <samiyelfrazier@gmail.com>
2024-11-10 12:24:04 -0800

    main is test

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,20 @@ Servos claw(1, BKND::P2D(0, 0), BKND::P2D(90, 1020));
 Sensors<BKND::sensors::type::Analog> /*sensor type*/
     startlight(0 /*port*/);
 PathFind navigate(tank.m_pass /*what wheels to read from*/);
-
+Sensors<BKND::sensors::type::Analog> dist(1);
 void Wait(float seconds) {
   tank.Brake();
   msleep(seconds * 1000);
 }
+float e = 2.71828;
+float pi = 3.14159265;
+float distance(float x) {
+  float stdDev = 4;
+  float mean = 4;
 
+  return 0.18 + (1.35 * (1.0 / (stdDev * std::sqrt(2 * pi))) *
+                 std::exp(-0.5 * std::pow((x - mean) / stdDev, 2)));
+}
 int main() {
   if (false /*tournament mode*/) {
     BKND::misc::waitforlight(startlight.m_Port);
@@ -22,14 +30,12 @@ int main() {
   BKND::Thread TIMER([]() { BKND::misc::Timer(); });
   TIMER.Run();
   VELOCITY.Run();
+  tank.Accelerate(100, 100, 15);
+  tank.Accelerate(0, 0, 15);
+  arm.GoTo(45, 1);
+  claw.Set(90);
+  navigate.GoTo(BKND::P2D(0, 0), 3);
   Wait(1);
-  tank.Accelerate(-100 /*left goal*/, -95 /*right goal*/,
-                  5 /*tics per second per second*/);
-  tank.Speed(-100, -95, 15);
-  tank.Accelerate(0, 0, 5);
-  // arm.GoTo(45 /*angle*/, 1 /*time*/);
-  // navigate.Face(-180, 2);
-  Wait(1);
-  std::cout << "hello!" << std::endl;
+  std::cout << "startlight reads " << startlight.Value() << std::endl;
   return 0;
 }
