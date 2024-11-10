241f199acf052dd97b28df85cebc70eec883d295
Samiyel Frazier <samiyelfrazier@gmail.com>
2024-11-10 14:36:48 -0800

    simplify main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,14 @@ Servos arm(0 /*port*/, BKND::P2D(-45 /*degrees*/, 125 /*tics*/) /*minimum*/,
 Servos claw(1, BKND::P2D(0, 0), BKND::P2D(90, 1020));
 Sensors<BKND::sensors::type::Analog> /*sensor type*/
     startlight(0 /*port*/);
-PathFind navigate(tank.m_pass /*what wheels to read from*/);
 Sensors<BKND::sensors::type::Analog> dist(1);
+PathFind navigate(tank.m_pass /*what wheels to read from*/);
+
 void Wait(float seconds) {
   tank.Brake();
   msleep(seconds * 1000);
 }
-float e = 2.71828;
-float pi = 3.14159265;
-float distance(float x) {
-  float stdDev = 4;
-  float mean = 4;
 
-  return 0.18 + (1.35 * (1.0 / (stdDev * std::sqrt(2 * pi))) *
-                 std::exp(-0.5 * std::pow((x - mean) / stdDev, 2)));
-}
 int main() {
   if (false /*tournament mode*/) {
     BKND::misc::waitforlight(startlight.m_Port);
