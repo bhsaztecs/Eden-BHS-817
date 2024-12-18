e3572ef97547642f2de804cb7b7d124348f2c10f
Samiyel Frazier <samiyelfrazier@gmail.com>
2024-12-17 17:47:09 -0800

    orientation in degrees

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@ Motors tank(3, 0, .97, 1.0, 5, 2.8);
 Servos arm(0, BKND::P2D(315, 0), BKND::P2D(1365, 90));
 Servos claw(1, BKND::P2D(0, 0), BKND::P2D(1000, 90));
 Sensors<BKND::sensors::type::Analog> startlight(1);
-PathFind navigate(tank.m_pass);
 Sensors<BKND::sensors::type::Analog> ground(0);
+PathFind navigate(tank.m_pass);
 void Wait(float seconds) {
   tank.Brake();
   msleep(seconds * 1000);
@@ -14,5 +14,9 @@ int main() {
   BKND::Thread TIMER([]() { BKND::misc::Timer(); });
   TIMER.Run();
   VELOCITY.Run();
+
+  navigate.Face(90, 1);
+  navigate.Face(0, 1);
+  tank.Brake();
   return 0;
 }
--- src/pathfind.cpp
+++ src/pathfind.cpp
@@ -17,13 +17,13 @@ void AngularPathfind(float p_deltal, float p_deltar, pass p_vals) {
   float wheelangle = (fabs(p_deltal) + fabs(p_deltar)) / 2;
   float orientationoffset = wheelangle / p_vals.turnrate;
 
-  BKND::G_Position.m_Orientation += orientationoffset;
+  BKND::G_Position.m_Orientation += BKND::Deg(orientationoffset);
 }
 
 void LinearPathfind(float p_deltal, float p_deltar, pass p_vals) {
   float distance = BKND::lerp(DTI, (p_deltal + p_deltar) / 2);
-  BKND::P2D delta(distance * cos(BKND::G_Position.m_Orientation),
-                  distance * sin(BKND::G_Position.m_Orientation));
+  BKND::P2D delta(distance * cos(BKND::Rad(BKND::G_Position.m_Orientation)),
+                  distance * sin(BKND::Rad(BKND::G_Position.m_Orientation)));
 
   BKND::G_Position.m_X += delta.m_X;
   BKND::G_Position.m_Y += delta.m_Y;
@@ -37,23 +37,16 @@ void DynamicPathfind(float p_deltal, float p_deltar) {
                              fabs(curvature) * sin(theta));
 
   BKND::G_Position += changeinposition;
-  BKND::G_Position.m_Orientation += theta;
+  BKND::G_Position.m_Orientation += BKND::Deg(theta);
 }
 void Face(float p_deg, float p_time, pass p_vals) {
   DBUG;
-  BKND::worldSpace originalposition = BKND::G_Position;
-  BKND::worldSpace origin;
-  BKND::G_Position = origin;
-  float deltaorientation = BKND::Rad(p_deg) - BKND::G_Position.m_Orientation;
-  float wheelangle = BKND::Deg(deltaorientation * (p_vals.turnrate / 2));
-  BKND::motors::Rotation(wheelangle, -wheelangle, p_time, p_vals);
-  BKND::G_Position += originalposition;
+  float deltaorientation = p_deg - BKND::G_Position.m_Orientation;
+  float wheelangle = deltaorientation * (p_vals.turnrate);
+  BKND::motors::Rotation(-wheelangle, wheelangle, p_time, p_vals);
 }
 void GoTo(BKND::P2D p_goal, float p_time, pass p_vals) {
   DBUG;
-  BKND::worldSpace originalposition = BKND::G_Position;
-  BKND::worldSpace origin;
-  BKND::G_Position = origin;
   BKND::P2D deltaposition(p_goal - BKND::G_Position);
   Face(deltaposition.Angle(), (p_time * deltaposition.Angle()), p_vals);
 
@@ -62,7 +55,6 @@ void GoTo(BKND::P2D p_goal, float p_time, pass p_vals) {
 
   BKND::motors::Distance(deltaposition.Magnitude(), deltaposition.Magnitude(),
                          p_time * deltaposition.Magnitude(), p_vals);
-  BKND::G_Position += originalposition;
 }
 } // namespace pathFind
 } // namespace BKND
